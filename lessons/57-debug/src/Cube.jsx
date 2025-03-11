d982827a2c8b797a13065fa74b876516d730c593
Alex Grieco <>
2025-03-11 08:44:29 -0700

    Lessons 57 and 58 and starter for 59

---
+++ lessons/57-debug/src/Cube.jsx
@@ -0,0 +1,7 @@
+export default function Cube({ scale = 1, visible = true }) {
+    return <mesh position-x={2} scale={scale} visible={visible}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+    </mesh>
+
+}
