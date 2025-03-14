9d70d9c78c929e0c8799aba7ba42ac3b2c2d07a1
Alex Grieco <>
2025-03-14 09:03:35 -0700

    Lesson 62

--- lessons/62-mouse-events/src/Experience.jsx
+++ lessons/62-mouse-events/src/Experience.jsx
@@ -1,37 +1,60 @@
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { meshBounds, OrbitControls, useGLTF } from '@react-three/drei'
 import { useRef } from 'react'
 
-export default function Experience()
-{
+
+export default function Experience() {
     const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
+
+    useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2
     })
 
+    const eventHandler = () => {
+        cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`)
+    }
+
+
+    const hamburger = useGLTF('./hamburger.glb')
     return <>
 
         <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+        <directionalLight position={[1, 2, 3]} intensity={4.5} />
+        <ambientLight intensity={1.5} />
 
-        <mesh position-x={ - 2 }>
+        <mesh position-x={- 2} onClick={(event) => event.stopPropagation()}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+        <mesh
+            ref={cube}
+            position-x={2}
+            raycast={meshBounds}
+            scale={1.5}
+            onClick={eventHandler}
+            onPointerEnter={() => { document.body.style.cursor = 'pointer' }}
+            onPointerLeave={() => { document.body.style.cursor = 'default' }}
+        >
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
+
+        <primitive
+            object={hamburger.scene}
+            scale={0.25}
+            position-y={0.5}
+            onClick={(event) => {
+                console.log('click')
+                event.stopPropagation()
+            }}
+        />
 
     </>
 }
